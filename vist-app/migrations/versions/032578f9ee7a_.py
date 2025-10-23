700f35a9510638f27eae52ac616e455d6d227c2f
Vikusha Kotikova <almost.a.mathematician@gmail.com>
2025-10-23 12:38:19 +0300

    remove .ds_store and add it to .gitignore

---
+++ vist-app/migrations/versions/032578f9ee7a_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 032578f9ee7a
+Revises: 049dcf2d8a7d
+Create Date: 2025-09-23 16:24:00.964665
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '032578f9ee7a'
+down_revision = '049dcf2d8a7d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
